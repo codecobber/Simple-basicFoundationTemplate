c8d80eb25baa12e2f77068eec85c61b4adbd9e47
Cobber <codecobber@gmail.com>
2018-02-09 12:12:45 +0000

    Add files via upload

---
+++ simple/index.php
@@ -0,0 +1,58 @@
+<?php include('includes/header.inc.php'); ?><div id='start'>
+
+
+       <div id="bck_1" class="bck"><div id="block_1" class="row column text-center rowNo13">  <h2>
+    Our Newest Products
+  </h2>
+  <hr></div></div><div id="bck_2" class="bck"><div id="block_2" class="row rowNo18"><div class="large-5 columns">
+<img class="imgPlace" src="http://placehold.it/400x300">
+</div>
+<div class="large-5 columns">
+<h1>Cronggle doobi dobble watsit</h1>
+<p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+
+</div>
+
+</div></div><div id="bck_3" class="bck"><div id="block_3" class="row rowNo18"><div class="large-5 columns">
+<img class="imgPlace" src="http://placehold.it/400x300">
+</div>
+<div class="large-5 columns">
+<h1>Cronggle doobi dobble watsit</h1>
+<p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+
+</div>
+
+</div></div><div id="bck_4" class="bck"><div id="block_4" class="row rowNo18"><div class="large-5 columns">
+<img class="imgPlace" src="http://placehold.it/400x300">
+</div>
+<div class="large-5 columns">
+<h1>Cronggle doobi dobble watsit</h1>
+<p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+
+</div>
+
+</div></div><div id="bck_5" class="bck"><div id="block_5" class="row rowNo23">        <div class="large-12 columns">
+            <p>Lorem ipsum dolor sit amet, nulla cras penatibus sodales nunc vestibulum ut, maecenas suscipit, mi sem dolor leo wisi id, leo vitae eget posuere purus aliquam morbi. Vulputate fusce sapien non, tristique in non amet ac vitae tristique, aliquam vivamus a, ipsum malesuada sagittis nulla, id praesent morbi rhoncus morbi luctus. Ligula odio enim sed nisl diam urna.
+          </p>
+       </div>
+</div></div><div id="bck_6" class="bck"><div id="block_6" class="row rowNo05">
+
+<div class="medium-6 columns medium-push-6">
+<img class="imgPlace" src="http://placehold.it/750x350">
+</div>
+<div class="medium-6 columns medium-pull-6 ">
+<h2>Our Agency, our selves.</h2>
+<p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.</p>
+</div>
+
+</div></div><div id="bck_7" class="bck"><div id="block_7" class="row rowNo05">
+
+<div class="medium-6 columns medium-push-6">
+<img class="imgPlace" src="http://placehold.it/750x350">
+</div>
+<div class="medium-6 columns medium-pull-6 ">
+<h2>Our Agency, our selves.</h2>
+<p>Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, orci ac euismod semper, magna diam porttitor mauris, quis sollicitudin sapien justo in libero. Vestibulum mollis mauris enim. Morbi euismod magna ac lorem rutrum elementum. Donec viverra auctor.</p>
+</div>
+
+</div></div></div><?php include('includes/footer.inc.php'); ?>
